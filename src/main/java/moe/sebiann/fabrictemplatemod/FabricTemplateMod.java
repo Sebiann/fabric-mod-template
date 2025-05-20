4876faee8bd9c53dd5787cb1dcb1f8a535968fd5
Sebastian <code@sebiann.moe>
2025-05-20 09:37:00 +0200

    :sparkles: Few updates
    Signed-off-by: Sebastian <code@sebiann.moe>

---
+++ src/main/java/moe/sebiann/fabrictemplatemod/FabricTemplateMod.java
@@ -0,0 +1,10 @@
+package moe.sebiann.fabrictemplatemod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricTemplateMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello Fabric world!");
+    }
+}
